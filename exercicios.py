01eb022a4adb44e1badbdd35b2641ea27a79c156
apollovsilva <48492008+apollovsilva@users.noreply.github.com>
2019-03-15 13:15:25 -0300

    Update exercicios.py

--- exercicios.py
+++ exercicios.py
@@ -1,4 +1,3 @@
-
 print('Cacular tempo médio por milha')
 
 print('Conversão km para milha')
@@ -8,3 +7,37 @@ y = float(input('valor de y: '))
 x = y/1.61
 
 print(x)
+
+print('Conversão min para hora')
+
+min =float (input('colocar valor em minuto: '))
+
+t1 = min/60
+
+print(t1)
+
+print('Conversão seg para hora')
+
+seg = float (input('colocar valor em segundos: '))
+
+t2 = seg/3600
+
+print(t2)
+
+print('tempo em horas')
+
+h = t1 + t2
+
+print('h = ',h)
+
+print('cálculo tempo por milha')
+
+tempmil = h/x
+
+print('tempmil = ', tempmil)
+
+print('cáculo da velocidade em milhas por hora')
+
+v = x/h
+
+print('v = ', v)
