406c78411b0fa324b3b9a5ea4a75d63c9021585d
apollovsilva <48492008+apollovsilva@users.noreply.github.com>
2019-04-08 15:23:42 -0300

    Add files via upload

---
+++ Aula 2/exercicio1_aula2.py
@@ -0,0 +1,11 @@
+largura = 17
+
+altura = 12.0
+
+delimitador = "."
+
+print(type(largura/2))
+print(type(largura/2.0))
+print(type(altura/2))
+print(type(1 + 2*5))
+print(type(delimitador*5))
---
+++ Aula 2/exercicio2_aula2.py
@@ -0,0 +1,7 @@
+pi = 3.14
+
+r = 5
+
+volume = (4/3)*pi*r**3
+
+print(volume)
