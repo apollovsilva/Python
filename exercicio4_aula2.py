49b659c5e1d8a1d3339aa1ba4eb2d30de70457cf
apollovsilva <48492008+apollovsilva@users.noreply.github.com>
2019-04-08 16:09:58 -0300

    Delete exercicio4_aula2.py

--- exercicio4_aula2.py
+++
@@ -1,9 +0,0 @@
-comp_onda = 632.8*10**(-9)     #metros
-
-dist_anteparo = 1.98           #metros
-
-esp_fendas = 0.250*10**(-3)    #metros
-
-delta_y = (comp_onda*dist_anteparo)/(esp_fendas)  #distância entre dois máximos de interferência consecutivos
-
-print("{0:.5f}".format(delta_y))
