084eca5cdc9bc9dc589d8915d128070c291b9a5f
apollovsilva <48492008+apollovsilva@users.noreply.github.com>
2019-04-26 19:57:36 -0300

    Add files via upload

---
+++ Aula 6/flower2.py
@@ -0,0 +1,86 @@
+import turtle
+import math
+
+def polyline(t, length, n, angle):
+    for i in range(n):
+        t.fd(length)
+        t.lt(angle)
+
+def arc(t, r, angle):
+    arc_length = 2 * math.pi * r * angle / 360 
+    n = int(arc_length / 3) + 1 
+    step_length = arc_length / n 
+    step_angle = float(angle) / n 
+    polyline(t, step_length, n, step_angle)
+
+
+def petal(t, r, angle):
+    """Draws a petal using two arcs.
+    t: Turtle
+    r: radius of the arcs
+    angle: angle (degrees) that subtends the arcs
+    """
+    for i in range(2):
+        arc(t, r, angle)
+        t.lt(180-angle)
+
+
+def flower(t, n, r, angle):
+    """Draws a flower with n petals.
+    t: Turtle
+    n: number of petals
+    r: radius of the arcs
+    angle: angle (degrees) that subtends the arcs
+    """
+    for i in range(n):
+        petal(t, r, angle)
+        t.lt(360.0/n)
+        
+
+
+#def move(t, length):
+ #   """Move Turtle (t) forward (length) units without leaving a trail.
+  #  Leaves the pen down.
+   # """
+    #t.pu()
+    #t.fd(length)
+    #t.pd()
+
+#desenhando o talo e folha
+
+def talo(t):
+    t.pensize(3)
+    t.color("brown")
+    t.rt(90)
+    t.fd(150)
+    t.pu()
+    t.lt(180)
+    t.fd(45)
+    t.pd()
+    t.rt(60)
+    t.color("green","green")
+    t.begin_fill()
+    petal(bob,55.0,55.0)
+    t.end_fill()
+    
+    
+    
+    
+    
+#jn = turtle.Screen()
+#jn.bgcolor("yellow")
+
+bob = turtle.Turtle()
+#bob.color("green")
+bob.speed(100)
+
+bob.color("midnight blue","dark orchid")
+bob.begin_fill()
+flower(bob, 10, 40.0, 100.0)
+bob.end_fill()
+talo(bob)
+
+bob.hideturtle()
+#Para salvar o desenho da tela num arquivo .ps   
+tc= turtle.Screen().getcanvas()
+tc.postscript(file="flores.ps")
